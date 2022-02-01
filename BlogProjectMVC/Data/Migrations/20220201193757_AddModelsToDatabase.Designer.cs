5ffe6818b2e11fad8f800b7b60fe10911cef94d5
Heagan Henry <76776249+heaganhenry@users.noreply.github.com>
2022-02-01 16:40:09 -0400

    Create tables in MSSQL

--- BlogProjectMVC/Data/Migrations/20220201193757_AddModelsToDatabase.Designer.cs
+++ BlogProjectMVC/Data/Migrations/20220201193757_AddModelsToDatabase.Designer.cs
@@ -4,14 +4,16 @@ using BlogProjectMVC.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogProjectMVC.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220201193757_AddModelsToDatabase")]
+    partial class AddModelsToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
